c2e65267179b164e068133e0fd1e8ee228bbe199
notfizz1 <omerabazi3@gmail.com>
2026-06-13 13:55:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'havenscroll-cache-v2.2.3';
+const CACHE_NAME = 'havenscroll-cache-v2.3.0';
 
 // Everything the sanctuary needs to run with zero network
 const ASSETS_TO_CACHE = [
